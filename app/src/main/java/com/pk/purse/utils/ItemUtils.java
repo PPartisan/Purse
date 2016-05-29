e41395396c1e318ffbb52026aa18405e6baa66a6
PPartisan <PPartisan@users.noreply.github.com>
2016-05-29 17:02:19 +0100

    Needs InputFilter for commas on EditText

--- app/src/main/java/com/pk/purse/utils/ItemUtils.java
+++ app/src/main/java/com/pk/purse/utils/ItemUtils.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 /**
  * Created by tom on 29/05/16.
  */
-public class ItemUtils {
+public final class ItemUtils {
 
     private ItemUtils() { throw new AssertionError(); }
 
